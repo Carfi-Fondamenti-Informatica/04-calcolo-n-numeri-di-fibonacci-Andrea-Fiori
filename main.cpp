62cb6b83558e78f107fdfd17e540cb00d6de1592
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-11-21 18:50:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() { 
+    int numero;
+    cin >> numero;
+    int risultato=0, a=1, b=1;
+    if (numero<2) {
+        cout << "errore";
+    }else {
+        cout << "1" << endl;
+        cout << "1" << endl;
+    for (int j = 0; j <= (numero-3); ++j) {
+        risultato=a+b;
+        cout << risultato << endl;
+        a=b;
+        b=risultato;
+    }
+    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    return 0;
 }
